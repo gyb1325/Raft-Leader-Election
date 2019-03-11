fb440a1ee2e14c16bcc1e595018f71ba377ee606
Yongbin Gu <guyongbin1325@gmail.com>
2019-03-10 22:39:36 -0700

    Write a script make it run on the MAC automatically

---
+++ script.sh
@@ -0,0 +1,20 @@
+osascript -e 'tell app "Terminal"
+    do script "cd Github/Raft-Leader-Election ; python run.py 1"
+end tell'
+
+osascript -e 'tell app "Terminal"
+    do script "cd Github/Raft-Leader-Election ; python run.py 2"
+end tell'
+
+
+osascript -e 'tell app "Terminal"
+    do script "cd Github/Raft-Leader-Election ; python run.py 3"
+end tell'
+
+osascript -e 'tell app "Terminal"
+    do script "cd Github/Raft-Leader-Election ; python run.py 4"
+end tell'
+
+osascript -e 'tell app "Terminal"
+    do script "cd Github/Raft-Leader-Election ; python run.py 5"
+end tell'
